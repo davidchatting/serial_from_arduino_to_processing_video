140cb4fd2603a0132dfa24a24a000a9680161941
David Chatting <david@davidchatting.com>
2013-11-03 16:36:56 +0000

    call & response protocol - stops serial buffer filling-up

--- arduino_serialout_pot/arduino_serialout_pot.ino
+++ arduino_serialout_pot/arduino_serialout_pot.ino
@@ -7,14 +7,12 @@ void setup(){
 }
 
 void loop(){
-
-  potValue = analogRead(potPin);                 // read the value from the pot
-  serialValue = map(potValue, 0, 1023, 0, 255);  // scale pot value to 0-255
-  
-  //Serial.println(serialValue);
-  Serial.write(serialValue);                     // send serial value to computer as a byte (not as a string like when we use println)
-
-  delay(50); // wait a little while before sending again (so we're not sending too much data to Processing)
+  if(Serial.available()>0){
+    Serial.read();
+    
+    potValue = analogRead(potPin);                 // read the value from the pot
+    serialValue = map(potValue, 0, 1023, 0, 255);  // scale pot value to 0-255
+    
+    Serial.write(serialValue);                     // send serial value to computer as a byte (not as a string like when we use println)
+  }
 }
-
-
